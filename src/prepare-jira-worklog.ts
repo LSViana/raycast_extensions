29743bd1d5b68394a7cb8fd1678ee302698dbf49
LSViana <lv201122@gmail.com>
2024-08-30 18:06:57 -0300

    Introduce the "Prepare Jira worklog" extension

---
+++ src/prepare-jira-worklog.ts
@@ -0,0 +1,19 @@
+import { Clipboard, showHUD } from "@raycast/api";
+
+export default async function main() {
+  const clipboardContent = await Clipboard.read()
+
+  if (!clipboardContent.text) {
+    await showHUD("No text in clipboard.");
+  }
+
+  const result = clipboardContent.text
+    .replace(/(DEV|TESTDOME)-[\d]+\s?/, '')
+    .split(";")
+    .map((item) => `- ${item}`)
+    .join("\n\n");
+
+  await Clipboard.copy(result);
+
+  await showHUD("Worklog copied to clipboard.");
+}
